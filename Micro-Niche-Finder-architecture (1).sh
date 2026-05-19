cd24e2d7436bee84beccf4aab2fac9a15d79ad7b
asadkhan7421 <asad.khan74213@gmail.com>
2026-05-19 11:46:46 +0500

    Add files via upload

---
+++ Micro-Niche-Finder-architecture (1).sh
@@ -0,0 +1,7 @@
+git clone https://github.com/YOUR_USERNAME/micro-niche-finder
+cd micro-niche-finder
+# Paste the setup script from Option 1
+# Then paste all code files from my previous reply
+git add .
+git commit -m "init"
+git push
